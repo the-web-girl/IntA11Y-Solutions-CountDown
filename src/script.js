94fead176550fe7833268c91f0534d54ecbac523
the-web-girl <angelique@web-i.org>
2025-04-01 00:48:39 +0200

    on va bouger l'html dans le js pour qu'il le charge

--- src/script.js
+++ src/script.js
@@ -10,7 +10,12 @@ let hours = Math.floor(distance % (1000*60*60*24) / (1000*60*60));
 let minutes = Math.floor(distance % (1000*60*60) / (1000*60));
 let seconds = Math.floor(distance % (1000*60) / (1000));
 // Vérification dans la console que les variables fonctionnent correctement
-console.log(days, hours, minutes, seconds)
+// console.log(days, hours, minutes, seconds)
 
+// Ce qui se trouve dans #id-time_countdown va venir ici cette partie de l'html sera créer via la js
+document.querySelector("#time_countdown").innerHTML = `
+
+
+`
 
 }, 1000);
